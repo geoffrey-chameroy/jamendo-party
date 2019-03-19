f8dbd645968b6258bcdd73549a5c371d9ff1edb0
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2019-03-19 21:41:03 +0100

    JAMP-20 - Create playlists component (#5)

---
+++ src/app/playlists/playlists.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playlists',
+  templateUrl: './playlists.component.html',
+  styleUrls: ['./playlists.component.less']
+})
+export class PlaylistsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
