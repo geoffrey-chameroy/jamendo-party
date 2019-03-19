4f3bfb3f0aab4df2ce8be3e1f46575e37bb522ec
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2019-03-19 18:57:38 +0100

    JAMP-7 - Create home page template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// App components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Shared components
+import { HeaderComponent } from './shared/header/header.component';
+
+// Page components
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
