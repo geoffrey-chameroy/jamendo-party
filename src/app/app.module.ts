41bf21327ee144ad1836f72cf6ea46a138a31dc2
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2019-03-19 20:57:49 +0100

    JAMP-19 - Create topbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './shared/navigation/navigation.component';
+import { TopbarComponent } from './shared/topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
