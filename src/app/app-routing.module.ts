c7b97160ccfb840ce1365dca38f740a6f0549f83
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2019-03-19 22:19:07 +0100

    JAMP-18 - Create playlist component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {PlaylistsComponent} from './playlists/playlists.component';
 import {HomeComponent} from './home/home.component';
+import {PlaylistComponent} from './playlist/playlist.component';
 
 const routes: Routes = [
     {path: 'start', component: HomeComponent},
+    {path: 'playlist', component: PlaylistComponent},
     {path: 'explore/playlists', component: PlaylistsComponent},
     {path: '**', redirectTo: '/start'}
 ];
---
+++ src/app/playlist/playlist.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist',
+  templateUrl: './playlist.component.html',
+  styleUrls: ['./playlist.component.less']
+})
+export class PlaylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
