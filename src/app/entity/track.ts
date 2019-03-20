e4921ee811ae31b5518a723dbdfecdadeb7f061b
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2019-03-20 23:42:59 +0100

    JAMP-25 - Display tracks and cover on playlists (#10)

---
+++ src/app/entity/track.ts
@@ -0,0 +1,15 @@
+export class Track {
+    id: string;
+    name: string;
+    album_id: string;
+    artist_id: string;
+    duration: string;
+    artist_name: string;
+    playlistadddate: string;
+    position: string;
+    license_ccurl: string;
+    album_image: string;
+    image: string;
+    audio: string;
+    audiodownload: string;
+}
--- src/app/service/playlist.service.ts
+++ src/app/service/playlist.service.ts
@@ -2,14 +2,28 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {config} from '../../config';
+import {Playlist} from '../entity/playlist';
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class PlaylistService {
     constructor(private http: HttpClient) { }
 
-    getPlaylistsObserver(): Observable<any> {
+    getPlaylistsObserver(): Observable<Playlist[]> {
         const params = '?client_id=' + config.clientId + '&order=creationdate_desc&namesearch=best of';
+        const url = config.apiUrl + 'playlists' + params;
 
-        return this.http.get(config.apiUrl + 'playlists' + params);
+        return this.http.get<any>(url).pipe(
+            map(({results}) => {
+                let playlists: Playlist[] = [];
+                for(let playlistJson of results) {
+                    let playlist = new Playlist();
+                    Object.assign(playlist, playlistJson);
+                    playlists.push(playlist);
+                }
+
+                return playlists;
+            })
+        );
     }
 }
