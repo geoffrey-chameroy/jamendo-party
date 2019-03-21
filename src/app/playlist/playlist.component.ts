cae17ccc523c162579574eaaee5192e1bac0b1e1
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2019-03-21 11:51:22 +0100

    JAMP-23 - Display tracks and playlist from API for playlist

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,13 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Playlist} from '../entity/playlist';
+import {Track} from '../entity/track';
+import {PlaylistService} from '../service/playlist.service';
+import {TrackService} from '../service/track.service';
 
 @Component({
-  selector: 'app-playlist',
-  templateUrl: './playlist.component.html',
-  styleUrls: ['./playlist.component.less']
+    selector: 'app-playlist',
+    templateUrl: './playlist.component.html',
+    styleUrls: ['./playlist.component.less']
 })
 export class PlaylistComponent implements OnInit {
+    playlist: Playlist;
+    tracks: Track[];
 
-  constructor() { }
+    constructor(
+        private route: ActivatedRoute,
+        private playlistService: PlaylistService,
+        private trackService: TrackService
+    ) {
+    }
 
-  ngOnInit() { }
+    ngOnInit() {
+        const playlistId = this.route.snapshot.paramMap.get('id');
+
+        this.getPlaylist(playlistId);
+        this.getTracks(playlistId);
+    }
+
+    getPlaylist(playlistId: string): Promise<any> {
+        return new Promise(resolve => {
+            this.playlistService.getPlaylistObserver(playlistId).subscribe(playlist => {
+                this.playlist = playlist;
+                resolve(this.playlist);
+            });
+        });
+    }
+
+    getTracks(playlistId: string): Promise<any> {
+        return new Promise(resolve => {
+            this.trackService.getTracksObserver(playlistId).subscribe(tracks => {
+                this.tracks = tracks;
+                resolve(this.tracks);
+            });
+        });
+    }
 }
